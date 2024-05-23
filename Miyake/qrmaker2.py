3e2c70607583dbb0b119204d467e948ba51edc3c
MokaMiyake <miyake.moka.dr@mynavi.jp>
2024-05-23 08:50:49 +0000

    QRコード作成プログラムの更新

--- Miyake/qrmaker2.py
+++ Miyake/qrmaker2.py
@@ -1,3 +1,4 @@
+#QRコードを大量作成するプログラム（やり直し）
 import qrcode
 
 a_file = open("qr_url.txt", encoding="utf-8")
@@ -6,11 +7,11 @@ url = a_file.readlines()
 for i in url:
 #QRコード作成
     img = qrcode.make(i)
-    img.save("/home/matcha-23training/projects/python/files/", {0}.png.format(i))
+    img.save("/home/matcha-23training/projects/python/files/{0}.png".format(i+1))
 
 #ファイル閉じる
 a_file.close()
 
 #ファイルの読み込み一行ずつにする
 #一行ずつQRコード作成
-#
+
